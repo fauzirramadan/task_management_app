8ec7ebaa2265f16229d3ded69f7702c8ac5342cd
fauzirramadan <fauzirramadan@gmail.com>
2022-10-18 14:28:32 +0700

    add notification

---
+++ lib/services/notification_services.dart
@@ -0,0 +1,84 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+
+class NotificationHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin(); //
+
+  initializeNotification() async {
+    //tz.initializeTimeZones();
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+            requestSoundPermission: false,
+            requestBadgePermission: false,
+            requestAlertPermission: false,
+            onDidReceiveLocalNotification: onDidReceiveLocalNotificationIos);
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings("appicon");
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      iOS: initializationSettingsIOS,
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
+
+  void displayNotification(
+      {required String title, required String body}) async {
+    log("doing test");
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'mytaskid', 'mytaskchannel',
+        importance: Importance.max, priority: Priority.high);
+    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'It could be anything you pass',
+    );
+  }
+
+  Future onDidReceiveLocalNotificationIos(
+      int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title!),
+        content: Text(body!),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('Ok'),
+            onPressed: () async {
+              Navigator.of(context, rootNavigator: true).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void requestIOSPermissions() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
+import 'package:task_management_app/services/notification_services.dart';
 import 'package:task_management_app/services/theme_services.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  @override
+  void initState() {
+    _notificationHelper;
+    _notificationHelper.initializeNotification();
+    _notificationHelper.requestIOSPermissions();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +28,11 @@ class _HomePageState extends State<HomePage> {
         leading: IconButton(
             onPressed: () {
               ThemeServices.switchTheme();
+              _notificationHelper.displayNotification(
+                  title: "Theme changed",
+                  body: Get.isDarkMode
+                      ? "Activate Light mode"
+                      : "Activate Dark mode");
             },
             icon: const Icon(Icons.mode_night_rounded)),
       ),
